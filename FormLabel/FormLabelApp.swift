72dbe51715e0bfe36ffb9b25c29262d002c4e324
aciosui <mrana.swe@gmail.com>
2024-03-03 19:14:51 -0500

    Initial Commit

---
+++ FormLabel/FormLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormLabelApp.swift
+//  FormLabel
+//
+//  Created by MD  on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FormLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
